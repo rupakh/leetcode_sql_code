93744c7e4958f616bd9f5941ac930376d5efd8e2
Rupak Hattikudur <49523143+rupakh@users.noreply.github.com>
2023-11-28 22:33:27 -0500

    Time: 875 ms (40.94%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,17 @@
-select w2.id
-from weather w1 join weather w2 
-on datediff(w1.recorddate,w2.recorddate)=-1
-where w2.temperature  > w1.temperature 
+# Write your MySQL query statement below
+WITH CTE AS (
+select
+id,
+coalesce(LEAD(id) OVER(ORDER BY recorddate ASC),0) AS id_next,
+recorddate,
+coalesce(LEAD(recorddate) OVER(ORDER BY recorddate ASC),0) AS recorddate_next,
+temperature,
+LEAD(temperature) OVER(ORDER BY recorddate ASC) AS temperature_next
+from weather
+)
+SELECT  id_next as Id 
+from cte
+where 
+temperature_next>temperature and 
+recorddate<recorddate_next  and 
+date_add(recorddate, INTERVAL 1 DAY)=recorddate_next
